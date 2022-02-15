0fb117ee0d0abad5db6f864b26ae1b22a3cecf42
easmile <easmile@hotmail.co.uk>
2022-02-15 15:00:45 +0000

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/ek/helloworld/Hello.java
@@ -0,0 +1,13 @@
+
+package com.ek.helloworld;
+
+/**
+ *
+ * @author Easmile
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Easmile!");
+        System.out.println("My name is");
+    }
+}
